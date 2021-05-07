ae35a57882e067bfafb6133c0cbe87c44e983441
Lucas Weyne <weynelucas@gmail.com>
2021-05-07 08:58:01 -0300

    feat(models): create first version of resource

--- rest_fhir/models.py
+++ rest_fhir/models.py
@@ -42,6 +42,25 @@ class Resource(models.Model):
         verbose_name = 'resource'
         verbose_name_plural = 'resources'
 
+    def save(self, resource_text=None, **kwargs):
+        first = self._state.adding
+        resource_id = str(self.resource_id)
+        resource_text = resource_text and {'id': resource_id, **resource_text}
+
+        super().save(**kwargs)
+        self.set_resource_version(resource_text=resource_text, first=first)
+
+    def set_resource_version(self, resource_text=None, first=False):
+        version = self.history.create(resource_text=resource_text)
+        self.resource_version = version
+        self.updated_at = version.published_at
+        if first:
+            self.published_at = version.published_at
+
+        super().save(
+            update_fields=['resource_version', 'updated_at', 'published_at']
+        )
+
 
 class ResourceVersion(models.Model):
     resource_version = models.AutoField(primary_key=True)
@@ -52,7 +71,13 @@ class ResourceVersion(models.Model):
         db_column='resource_id',
     )
     resource_text = models.JSONField(
-        help_text=_('The actual full text of the resource being stored.')
+        help_text=_('The actual full text of the resource being stored')
+    )
+    published_at = models.DateTimeField(
+        auto_now_add=True,
+        help_text=_(
+            'Date that version was created version of the resource was created'
+        ),
     )
 
     class Meta:
---
+++ tests/test_models.py
@@ -0,0 +1,44 @@
+from django.test import TestCase
+
+from rest_fhir.models import Resource
+
+
+class ResourceTestCase(TestCase):
+    def test_create_resource(self):
+        resource_text = {
+            'resourceType': 'Patient',
+            'name': [
+                {
+                    'use': 'official',
+                    'family': 'Donald',
+                    'given': [
+                        'Duck',
+                    ],
+                }
+            ],
+        }
+
+        resource = Resource()
+        resource.save(resource_text=resource_text)
+
+        resource_text_with_id = {
+            'id': str(resource.resource_id),
+            **resource_text,
+        }
+
+        self.assertIsNotNone(resource.resource_version_id)
+        self.assertEqual(
+            resource.resource_version.resource_text,
+            resource_text_with_id,
+        )
+        self.assertEqual(
+            resource.published_at,
+            resource.resource_version.published_at,
+        )
+        self.assertEqual(
+            resource.updated_at,
+            resource.resource_version.published_at,
+        )
+
+        self.assertEqual(resource.history.count(), 1)
+        self.assertEqual(resource.resource_version, resource.history.first())
